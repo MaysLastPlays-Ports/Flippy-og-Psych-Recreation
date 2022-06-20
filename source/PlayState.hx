4d75246058b2b075c926e7034977cb8b8011f0b6
MaysLastPlayYT <106990059+MaysLastPlayYT@users.noreply.github.com>
2022-06-20 13:05:52 +0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1336,7 +1336,7 @@ class PlayState extends MusicBeatState
 		char.y += char.positionArray[1];
 	}
 
-public function startVideo(name:String)
+        public function startVideo(name:String)
 	{
 		#if VIDEOS_ALLOWED
 		inCutscene = true;
